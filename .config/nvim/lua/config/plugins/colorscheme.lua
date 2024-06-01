3372c0c8a81e6a5ae6def0bea8852ad70157237e
Johannes Huber <johu@gentoo.org>
2024-06-01 10:54:26 +0200

    nvim: improve colorscheme
    Adds a plugin that assigns dev icon colors based on the general color
    scheme.

--- .config/nvim/lua/config/plugins/colorscheme.lua
+++ .config/nvim/lua/config/plugins/colorscheme.lua
@@ -1,24 +1,39 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    require("catppuccin").setup({
-      flavour = "mocha",
-      transparent_background = true,
-      integrations = {
-        alpha = true,
-        cmp = true,
-        dap = true,
-        dap_ui = true,
-        indent_blankline = true,
-        mason = true,
-        telescope = true,
-        treesitter = true,
-        which_key = true,
-      },
-    })
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+        transparent_background = true,
+        integrations = {
+          alpha = true,
+          cmp = true,
+          dap = true,
+          dap_ui = true,
+          indent_blankline = true,
+          mason = true,
+          telescope = true,
+          treesitter = true,
+          which_key = true,
+        },
+      })
 
-    vim.cmd.colorscheme("catppuccin")
-  end,
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+  {
+    "rachartier/tiny-devicons-auto-colors.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    event = "VeryLazy",
+    config = function()
+      local theme_colors = require("catppuccin.palettes").get_palette("macchiato")
+      require("tiny-devicons-auto-colors").setup({
+        colors = theme_colors,
+      })
+    end,
+  },
 }
