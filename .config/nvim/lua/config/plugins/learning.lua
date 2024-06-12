d5fa3a8bf3376d4d516ba73c19ff3bef9dcde773
Johannes Huber <johu@gentoo.org>
2024-06-12 21:56:19 +0200

    nvim: hardtime plugin
    Should decrease bad vim movement habbits.

--- .config/nvim/lua/config/plugins/learning.lua
+++ .config/nvim/lua/config/plugins/learning.lua
@@ -3,4 +3,12 @@ return {
     'ThePrimeagen/vim-be-good',
     event = 'VeryLazy',
   },
+  {
+    'm4xshen/hardtime.nvim',
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'nvim-lua/plenary.nvim',
+    },
+    opts = {},
+  },
 }
