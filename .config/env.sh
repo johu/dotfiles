34fd298ad598af0de34e5eea9f55b3fa236c20a1
Johannes Huber <johu@gentoo.org>
2024-09-14 15:33:07 +0200

    env: more user home cleanup

--- .config/env.sh
+++ .config/env.sh
@@ -34,8 +34,11 @@ export GNUPGHOME="$HOME/.config/gnupg"
 export PASSWORD_STORE_DIR="$HOME/.local/share/password-store"
 export GTK2_RC_FILES="$HOME/.config/gtk-2.0/gtkrc-2.0"
 export WINEPREFIX="$XDG_DATA_HOME/wineprefixes/default"
+export WGETRC="$XDG_CONFIG_HOME/wgetrc"
+export ICEAUTHORITY="$XDG_CACHE_HOME"/ICEauthority
 export _JAVA_OPTIONS=-Djava.util.prefs.userRoot="$XDG_CONFIG_HOME"/java
 export MYSQL_HISTFILE="$XDG_DATA_HOME"/mysql_history
+export CUDA_CACHE_PATH="$XDG_CACHE_HOME"/nv
 export AWS_SHARED_CREDENTIALS_FILE="$XDG_CONFIG_HOME"/aws/credentials
 export AWS_CONFIG_FILE="$XDG_CONFIG_HOME"/aws/config
 export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/npmrc
