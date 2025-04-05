e4ef3c3a0e1d42cda0f957564e0730e1d081bef8
Johannes Huber <johu@gmx.de>
2025-04-05 22:20:50 +0200

    nvim: status line improvements

--- .config/nvim/lua/plugins/statusline.lua
+++ .config/nvim/lua/plugins/statusline.lua
@@ -6,7 +6,15 @@ return {
   config = function()
     require('lualine').setup {
       options = {
-        theme = 'catppuccin',
+        theme = 'auto',
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'lsp_status', 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
       },
     }
   end,
