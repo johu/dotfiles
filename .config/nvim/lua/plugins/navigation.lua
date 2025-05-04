ed921fc23c46f1bff700c665c0e3dc667a1beeb9
Johannes Huber <johu@gmx.de>
2025-05-04 20:38:57 +0200

    nvim: move telescope to navigation

--- .config/nvim/lua/plugins/navigation.lua
+++ .config/nvim/lua/plugins/navigation.lua
@@ -53,4 +53,67 @@ return {
       vim.keymap.set('n', '-', oil.toggle_float, {})
     end,
   },
+  {
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+      'nvim-tree/nvim-web-devicons',
+      'folke/todo-comments.nvim',
+    },
+    config = function()
+      local telescope = require 'telescope'
+      local actions = require 'telescope.actions'
+      local transform_mod = require('telescope.actions.mt').transform_mod
+
+      local trouble = require 'trouble'
+      local trouble_telescope = require 'trouble.sources.telescope'
+
+      -- or create your custom action
+      local custom_actions = transform_mod {
+        open_trouble_qflist = function(prompt_bufnr)
+          trouble.toggle 'quickfix'
+        end,
+      }
+
+      telescope.setup {
+        defaults = {
+          path_display = { 'smart' },
+          mappings = {
+            i = {
+              ['<c-k>'] = actions.move_selection_previous, -- move to prev result
+              ['<c-j>'] = actions.move_selection_next, -- move to next result
+              ['<c-q>'] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
+              ['<c-t>'] = trouble_telescope.open,
+            },
+          },
+        },
+        extensions = {
+          fzf = {},
+        },
+      }
+
+      telescope.load_extension 'fzf'
+
+      local functions = require 'telescope.builtin'
+      local keymap = vim.keymap
+      keymap.set('n', '<leader>fc', functions.grep_string, { desc = 'find string under cursor in cwd' })
+      keymap.set('n', '<leader>ff', functions.find_files, { desc = 'find files in cwd' })
+      keymap.set('n', '<leader>fh', functions.help_tags, { desc = 'find help' })
+      keymap.set('n', '<leader>fr', functions.oldfiles, { desc = 'find recent files' })
+      keymap.set('n', '<leader>fs', functions.live_grep, { desc = 'find string in cwd' })
+      keymap.set('n', '<leader>ft', '<cmd>todotelescope<cr>', { desc = 'find todos' })
+      keymap.set('n', '<leader>en', function()
+        functions.find_files {
+          cwd = vim.fn.stdpath 'config',
+        }
+      end, { desc = 'edit neovim config' })
+      keymap.set('n', '<leader>ep', function()
+        functions.find_files {
+          cwd = vim.fs.joinpath(vim.fn.stdpath 'data', 'lazy'),
+        }
+      end, { desc = 'edit packages' })
+    end,
+  },
 }
