298c8d04110c407656fbeacc91d5f56436065b8f
Johannes Huber <johu@gmx.de>
2025-05-04 18:42:40 +0200

    nvim: improve health check

--- .config/nvim/lua/config/health.lua
+++ .config/nvim/lua/config/health.lua
@@ -1,3 +1,9 @@
+local start = vim.health.start
+local ok = vim.health.ok
+local info = vim.health.info
+local warn = vim.health.warn
+local error = vim.health.error
+
 local check_version = function()
   local fmt_ver = function(v)
     return string.format('%s.%s.%s', v[1], v[2], v[3])
@@ -6,19 +12,30 @@ local check_version = function()
   local v1 = { vim.version().major, vim.version().minor, vim.version().patch }
   local v2 = { 0, 11, 0 }
   if vim.version.ge(v1, v2) then
-    vim.health.ok(string.format('Neovim version is: %s (>=%s is required)', fmt_ver(v1), fmt_ver(v2)))
+    ok(('Neovim version is: %s (>=%s is required)'):format(fmt_ver(v1), fmt_ver(v2)))
   else
-    vim.health.error(string.format('Neovim version is out of date: %s (>=%s is required)', fmt_ver(v1), fmt_ver(v2)))
+    error(('Neovim version is out of date: %s (>=%s is required)'):format(fmt_ver(v1), fmt_ver(v2)))
   end
 end
 
 local check_external_reqs = function()
-  for _, exe in ipairs { 'git', 'make', 'unzip', 'rg' } do
-    local is_executable = vim.fn.executable(exe) == 1
-    if is_executable then
-      vim.health.ok(string.format("Found executable: '%s'", exe))
+  for _, cmd in ipairs { 'git', 'make', 'unzip', 'curl', 'rg', 'fzf', { 'fd', 'fdfind' } } do
+    local name = type(cmd) == 'string' and cmd or vim.inspect(cmd)
+    local commands = type(cmd) == 'string' and { cmd } or cmd
+    ---@cast commands string[]
+    local found = false
+
+    for _, c in ipairs(commands) do
+      if vim.fn.executable(c) == 1 then
+        name = c
+        found = true
+      end
+    end
+
+    if found then
+      ok(("'%s' is installed"):format(name))
     else
-      vim.health.warn(string.format("Could not find executable: '%s'", exe))
+      warn(("'%s' is not installed"):format(name))
     end
   end
 
@@ -27,10 +44,10 @@ end
 
 return {
   check = function()
-    vim.health.start 'config'
+    start 'config'
 
     local uv = vim.uv or vim.loop
-    vim.health.info('System Information: ' .. vim.inspect(uv.os_uname()))
+    info('System Information: ' .. vim.inspect(uv.os_uname()))
 
     check_version()
     check_external_reqs()
