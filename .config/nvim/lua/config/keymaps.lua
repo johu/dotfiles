57e5290c074ccebf234e4642d353d6db8d34eb77
Johannes Huber <johu@gmx.de>
2025-05-05 20:39:57 +0200

    nvim: more keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,6 +3,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
 
 keymap.set('i', 'jk', '<ESC>', { desc = 'Exit instert mode with jk' })
 keymap.set('n', 'x', '"_x') -- delete without saving in buffer
@@ -27,11 +28,27 @@ keymap.set('n', '<leader>tn', ':tabn<CR>', { desc = 'Go to next tab' })
 keymap.set('n', '<leader>tp', ':tabp<CR>', { desc = 'Go to previous tab' })
 keymap.set('n', '<leader>tf', '<cmd>tabnew %<CR>', { desc = 'Open current buffer in new tab' })
 
+-- move visual selection
+keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move lines down in visual selection' })
+keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move lines up in visual selection' })
+
 -- remaps for centering
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
-vim.keymap.set('n', 'n', 'nzzzv')
-vim.keymap.set('n', 'N', 'Nzzzv')
+keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Move down in buffer with cursor centered' })
+keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Move up in buffer with cursor centered' })
+keymap.set('n', 'n', 'nzzzv')
+keymap.set('n', 'N', 'Nzzzv')
+keymap.set('n', 'J', 'mzJ`z')
+
+-- indent visual selection
+keymap.set('v', '<', '<gv', opts)
+keymap.set('v', '>', '>gv', opts)
 
 -- remap for keeping yank content after paste
-vim.keymap.set('x', '<leader>p', [["_dP]])
+keymap.set('x', '<leader>p', [["_dP]])
+keymap.set('v', 'p', '"_dp', opts)
+
+-- copies or yank to system clipboard
+keymap.set('n', '<leader>Y', [["+Y]], opts)
+
+-- leader d delete wont remember as yanked/clipboard when delete pasting
+keymap.set({ 'n', 'v' }, '<leader>d', [["_d]])
