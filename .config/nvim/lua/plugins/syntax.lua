c07023d164dfe1090ca953600bbdbf36fccddb81
Johannes Huber <johu@gmx.de>
2025-05-10 15:30:28 +0200

    nvim: change incremental selection key mapping

--- .config/nvim/lua/plugins/syntax.lua
+++ .config/nvim/lua/plugins/syntax.lua
@@ -15,10 +15,10 @@ return {
       incremental_selection = {
         enable = true,
         keymaps = {
-          init_selection = '<C-space>',
-          node_incremental = '<C-space>',
+          init_selection = '<Enter>',
+          node_incremental = '<Enter>',
           scope_incremental = false,
-          node_decremental = '<bs>',
+          node_decremental = '<BS>',
         },
       },
       ensure_installed = {
