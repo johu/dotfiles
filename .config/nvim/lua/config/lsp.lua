e2701d6bf6a6f23c63eb66a403ce93072928eee7
Johannes Huber <johu@gmx.de>
2025-05-11 21:10:14 +0200

    nvim: 0.11 cleanup
    The health check requires at least 0.11, therefore drop the helper
    obsolete functions.

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -17,22 +17,13 @@ vim.api.nvim_create_autocmd('LspAttach', {
     map('gW', fzf.lsp_live_workspace_symbols, 'Open Workspace Symbols')
     map('grt', fzf.lsp_typedefs, '[G]oto [T]ype Definition')
 
-    -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
-    local function client_supports_method(client, method, bufnr)
-      if vim.fn.has 'nvim-0.11' == 1 then
-        return client:supports_method(method, bufnr)
-      else
-        return client.supports_method(method, { bufnr = bufnr })
-      end
-    end
-
     -- The following two autocommands are used to highlight references of the
     -- word under your cursor when your cursor rests there for a little while.
     --    See `:help CursorHold` for information about when this is executed
     --
     -- When you move your cursor, the highlights will be cleared (the second autocommand).
     local client = vim.lsp.get_client_by_id(event.data.client_id)
-    if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+    if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
       local highlight_augroup = vim.api.nvim_create_augroup('nvim-lsp-highlight', { clear = false })
       vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
         buffer = event.buf,
@@ -55,7 +46,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
       })
     end
 
-    if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+    if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
       map('<leader>th', function()
         vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
       end, '[T]oggle Inlay [H]ints')
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -54,7 +54,7 @@ opt.splitbelow = true
 
 -- whitespace characters
 opt.list = true
-opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- decrease update time
 opt.updatetime = 250
@@ -75,7 +75,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
-    vim.highlight.on_yank()
+    vim.hl.on_yank()
   end,
 })
 
