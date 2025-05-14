d350c633c44371726036efae4708301b3658f487
Johannes Huber <johu@gmx.de>
2025-05-14 23:16:41 +0200

    nvim: dashboard recent files/projects

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -105,15 +105,12 @@ return {
       dashboard = {
         preset = {
           header = [[
-
-          ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗         
-          ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║         
-          ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║         
-          ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║         
-          ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║         
-          ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝         
-
-          ]],
+███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
           ---@type snacks.dashboard.Item[]
           keys = {
             { icon = ' ', key = 'f', desc = 'Find File', action = ":lua Snacks.dashboard.pick('files')" },
@@ -125,6 +122,13 @@ return {
             { icon = '󰒲 ', key = 'l', desc = 'Lazy', action = ':Lazy' },
             { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
           },
+        },
+        sections = {
+          { section = 'header' },
+          { icon = ' ', title = 'Keymaps', section = 'keys', indent = 2, padding = 1 },
+          { icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
+          { icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
+          { section = 'startup' },
         },
       },
     },
