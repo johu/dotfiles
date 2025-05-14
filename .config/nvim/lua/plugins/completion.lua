9dcbd28977b543b262aa2dfbbec987d71782db99
Johannes Huber <johu@gmx.de>
2025-05-14 20:08:14 +0200

    nvim: improve completion
    - alternate key mappings
    - command line ghost text

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -9,11 +9,26 @@ return {
     --- @module 'blink.cmp'
     --- @type blink.cmp.Config
     opts = {
-      keymap = { preset = 'default' },
+      keymap = {
+        preset = 'default',
+        ['<CR>'] = { 'accept', 'fallback' },
+        ['<C-j>'] = { 'select_next', 'fallback' },
+        ['<C-k>'] = { 'select_prev', 'fallback' },
+      },
       appearance = {
         nerd_font_variant = 'mono',
       },
-      completion = { documentation = { auto_show = true, auto_show_delay_ms = 500 } },
+      completion = {
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 500,
+        },
+      },
+      cmdline = {
+        completion = {
+          ghost_text = { enabled = true },
+        },
+      },
       sources = {
         default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
         providers = {
