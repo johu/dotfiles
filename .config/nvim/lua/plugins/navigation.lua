fa77691533b56d26adf9fb1745d627cdc537d82a
Johannes Huber <johu@gmx.de>
2025-06-15 11:21:56 +0200

    nvim: more fzf keymaps

--- .config/nvim/lua/plugins/navigation.lua
+++ .config/nvim/lua/plugins/navigation.lua
@@ -80,24 +80,35 @@ return {
     keys = function()
       local fzf = require 'fzf-lua'
       local keys = {
+        { '<c-j>', '<c-j>', ft = 'fzf', mode = 't', nowait = true },
+        { '<c-k>', '<c-k>', ft = 'fzf', mode = 't', nowait = true },
         -- find
-        { '<leader>fb', function() fzf.buffers() end, desc = 'Buffer', },
+        { '<leader>fb', function() fzf.buffers() end, desc = 'Buffers', },
         { '<leader>ff', function() fzf.files() end, desc = 'Files', },
+        { '<leader>fg', function() fzf.git_files() end, desc = 'Git Files', },
         { '<leader>fk', function() fzf.keymaps() end, desc = 'Keymaps', },
         { '<leader>fh', function() fzf.helptags() end, desc = 'Help', },
         { '<leader>fr', function() fzf.oldfiles() end, desc = 'Recent', },
         { '<leader>fu', function() fzf.builtin() end, desc = 'Builtin', },
         -- search
+        { '<leader>sb', function() fzf.lgrep_curbuf() end, desc = 'Buffer', },
         { '<leader>sg', function() fzf.live_grep() end, desc = 'Grep', },
         { '<leader>sw', function() fzf.grep_cword() end, desc = 'Current Word', },
         { '<leader>sW', function() fzf.grep_cWORD() end, desc = 'Current WORD', },
         { '<leader>sd', function() fzf.diagnostics_document() end, desc = 'Diagnostics', },
+        { '<leader>sD', function() fzf.diagnostics_workspace() end, desc = 'Diagnostics (Workspace)', },
         { '<leader>sR', function() fzf.resume() end, desc = 'Resume', },
+        -- git
+        { '<leader>gc', function() fzf.git_commits() end, desc = 'Commits', },
+        { '<leader>gs', function() fzf.git_status() end, desc = 'Status', },
         -- edit
         { '<leader>ec', function() fzf.files { cwd = vim.fn.stdpath 'config' } end, desc = 'Neovim Config', },
         { '<leader>ep', function() fzf.files { cwd = vim.fs.joinpath(vim.fn.stdpath 'config', 'lazy') } end, desc = 'Packages', },
         -- misc
-        { '<leader>/', function() fzf.lgrep_curbuf() end, desc = '[/] Find String in current Buffer', },
+        { '<leader>/', function() fzf.live_grep() end, desc = '[/] Grep', },
+        { '<leader>,', function() fzf.buffers { sort_mru = true, sort_lastused = true } end, desc = 'Switch Buffer', },
+        { '<leader>:', function() fzf.command_history() end, desc = 'Command History', },
+        { '<leader><leader>', function() fzf.files() end, desc = 'Find Files' },
       }
       return keys
     end,
