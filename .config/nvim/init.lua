4b2f3502b01e65e99f5fee7722f382dbe27b817c
Johannes Huber <johu@gmx.de>
2026-04-14 19:40:48 +0200

    nvim: move diagnostics

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,6 +2,7 @@ vim.g.config_start_time = vim.uv.hrtime()
 
 require 'config.options'
 require 'config.keymaps'
+require 'config.diagnostics'
 require 'config.autocmds'
 require('config.pack').setup()
 require 'config.lsp'
